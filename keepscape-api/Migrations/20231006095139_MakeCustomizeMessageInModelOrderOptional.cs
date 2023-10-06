b5adf22b44d08f4a87882b5871747be8cea5f231
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-10-06 17:52:36 +0800

    Make CustomizedMessage nullable

---
+++ keepscape-api/Migrations/20231006095139_MakeCustomizeMessageInModelOrderOptional.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace keepscape_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCustomizeMessageInModelOrderOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomizedMessage",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomizedMessage",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
